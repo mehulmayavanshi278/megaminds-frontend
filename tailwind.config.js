27ecb3cae80108c117c3e99538c3e5ee4c313c0c
mehulmayavanshi278 <mehulmayavanshi0867@gmail.com>
2024-05-30 01:51:49 +0530

    verce.config.css added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,26 @@ module.exports = {
   ],
   theme: {
     extend: {
-      boxShadow: {
-        custom: 'rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px',
-        normal:'rgba(149, 157, 165, 0.2) 0px 8px 24px;',
-      }
+      
     },
   },
   plugins: [],
 }
+
+
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/**/*.{js,jsx,ts,tsx}",
+//   ],
+//   theme: {
+//     extend: {
+//       boxShadow: {
+//         custom: 'rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px',
+//         normal:'rgba(149, 157, 165, 0.2) 0px 8px 24px;',
+//       }
+//     },
+//   },
+//   plugins: [],
+// }
